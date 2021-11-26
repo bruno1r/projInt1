ccbb5439ab6ad04bb8b72ad43c735c115be74a7f
bruno1r <2004100@aluno.univesp.br>
2021-11-26 13:46:52 -0300

    desenvolvimento

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Person, Documento, Venda, Produto
+from .models import Person
 
 
 admin.site.register(Person)
-admin.site.register(Documento)
-admin.site.register(Venda)
-admin.site.register(Produto)
+
